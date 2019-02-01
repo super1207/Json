2f5e489367568eed289ef11f6afbd4eba963b4bb
super1207 <30586004+super1207@users.noreply.github.com>
2019-02-01 13:34:32 +0800

    修复少量bug
    修复少量bug

--- sujson.h
+++ sujson.h
@@ -14,8 +14,10 @@ namespace SU {
 		Json();
 		Json(const std::string & str);
 		Json(const std::vector<std::string> & vec);
-		Json(const std::map<std::string, std::vector<std::string>> & m);
+		Json(const std::vector<Json> & vec);
+		Json(const std::map<std::string, Json> & m);
 		Json(double number);
+		Json(int number);
 		Json(void * null);
 		Json(bool B);
 		Json at(const std::string& key)const;
@@ -26,13 +28,16 @@ namespace SU {
 		double asDouble()const;
 		bool asBool()const;
 		bool isNull()const;
-		std::map<std::string, std::vector<std::string>> asMap()const;
-		std::vector<std::vector<std::string>> asVec()const;
+		std::map<std::string, Json> asMap()const;
+		std::vector<Json> asVec()const;
 		Json & operator<<(const std::string & str);
 		Json & operator>>(int & out);
-		friend std::ostream& operator <<(std::ostream&, const Json&);
-	private:
+		Json & operator>>(std::string & out);
+		Json & operator>>(double & out);
+		Json & operator>>(bool & out);
+		Json & operator>>(void *& out);
 		std::string toString()const;
+		friend std::ostream& operator <<(std::ostream&, const Json&);
 		std::vector<std::string> data;
 	};
 	std::ostream & operator<<(std::ostream &os, const Json  &ob);
